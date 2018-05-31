045a9792e69ca597e4f967b997628f27f29f9e06
dchan3 <dchan3@hawaii.edu>
2018-05-30 22:12:01 -1000

    Add linter and Travis tests

--- src/zh.js
+++ src/zh.js
@@ -1,4 +1,9 @@
 const data = require('../data/data_map.json');
 
 module.exports = { data: data, toSlug: (string) =>
-  string.split('').map((n) => Object.keys(data).includes(n) ? (data[n] + "-") : n).join("").replace(/-+$/, "") };
+  string.split('').map((n, i, a) => {
+    return (Object.keys(data).includes(n) ? (data[n] + '-') :
+      n + (i > 0 && i < a.length - 1 &&
+      Object.keys(data).includes(a[i + 1]) ? '-' : ''));
+  }).join('').replace(/-+$/, '')
+}
--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,14 @@
 var omnislug = require('../index.js'),
-      expect = require('chai').expect;
+  expect = require('chai').expect;
 
 describe('Chinese (zh)', function() {
   it('hanzi only string', function(done) {
-    expect(omnislug.toSlug("恭喜發財")).to.be.equal("gong-xi-fa-cai");
+    expect(omnislug.toSlug('恭喜發財')).to.be.equal('gong-xi-fa-cai');
+    done();
+  });
+
+  it('hanzi mixed with latin letters', function(done) {
+    expect(omnislug.toSlug('大家3Q吧')).to.be.equal('da-jia-3Q-ba');
     done();
   });
 });
